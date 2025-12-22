3458833305d612aa6cc5061f26e05789d03b7a45
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-22 22:27:10 +0600

    create function for generate color

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const btn_container = document.getElementById('btn_container') as HTMLElement;
+
+const random = () =>{
+    const r = Math.round(Math.random()*255);
+    const g = Math.round(Math.random()*255);
+    const b = Math.round(Math.random()*255);
+
+}
