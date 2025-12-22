22d98c9d93fcf2802e9bb781d9e4b14dafdaf09e
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-22 22:37:03 +0600

    feat:adding logic

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 const btn_container = document.getElementById('btn_container') as HTMLElement;
 
-const random = () =>{
+const randomColorGenerator = () =>{
     const r = Math.round(Math.random()*255);
     const g = Math.round(Math.random()*255);
     const b = Math.round(Math.random()*255);
 
+    return`(${r},${g},${b})`;
+
 }
+
+btn_container.addEventListener('click',(event) =>{
+    if (event.target.tagName==='BUTTON') {
+        
+    }
+})
