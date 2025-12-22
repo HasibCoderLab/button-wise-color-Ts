db7759d97ea5388f70f74cfa801f28bb4b1a49cb
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-22 21:11:01 +0600

    .

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,1 @@
-const app = document.getElementById("app");
-
-if (app) {
-  app.innerHTML = `
-    <button class="px-4 py-2 bg-blue-600 text-white rounded">
-      Tailwind + TS OK
-    </button>
-  `;
-}
+import './style.css'
