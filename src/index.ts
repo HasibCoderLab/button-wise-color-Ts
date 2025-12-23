0f141c27d80ebc2145e814f01c3e83f158a76efc
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-23 08:47:39 +0600

    update colors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 const btn_container = document.getElementById('btn_container') as HTMLElement;
 
-const randomColorGenerator = () =>{
+const randomColorGenerator = ():string =>{
     const r = Math.round(Math.random()*255);
     const g = Math.round(Math.random()*255);
     const b = Math.round(Math.random()*255);
@@ -9,8 +9,23 @@ const randomColorGenerator = () =>{
 
 }
 
-btn_container.addEventListener('click',(event) =>{
-    if (event.target.tagName==='BUTTON') {
-        
+btn_container.addEventListener('click',(event:MouseEvent) =>{
+    const target = event.target as HTMLElement
+    if (target.tagName==='BUTTON') {
+      let color =   target.textContent?.trim().toLowerCase();
+        if (color === 'green') {
+            document.body.style.backgroundColor = 'green'
+        }
+        else if (color === 'blue') {
+            document.body.style.backgroundColor = 'blue'
+        }
+        else if (color === 'red') {
+            document.body.style.backgroundColor = 'red'
+        } else if (color === 'yellow') {
+            document.body.style.backgroundColor = 'yellow'
+        } else {
+            document.body.style.backgroundColor = randomColorGenerator()
+
+        }
     }
 })
