bcc5defa4e696bcd52b37e76bd6372bf8d08c19f
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-12-23 09:22:10 +0600

    feat:include new logic

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,30 @@
 const btn_container = document.getElementById('btn_container') as HTMLElement;
 
-const randomColorGenerator = ():string =>{
-    const r = Math.round(Math.random()*255);
-    const g = Math.round(Math.random()*255);
-    const b = Math.round(Math.random()*255);
+const randomColorGenerator = (): string => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
 
-    return`(${r},${g},${b})`;
-
+    return `rgb(${r}, ${g}, ${b})`;
 }
 
-btn_container.addEventListener('click',(event:MouseEvent) =>{
+btn_container.addEventListener('click', (event: MouseEvent) => {
     const target = event.target as HTMLElement
-    if (target.tagName==='BUTTON') {
-      let color =   target.textContent?.trim().toLowerCase();
-        if (color === 'green') {
-            document.body.style.backgroundColor = 'green'
-        }
-        else if (color === 'blue') {
-            document.body.style.backgroundColor = 'blue'
-        }
-        else if (color === 'red') {
-            document.body.style.backgroundColor = 'red'
-        } else if (color === 'yellow') {
-            document.body.style.backgroundColor = 'yellow'
-        } else {
-            document.body.style.backgroundColor = randomColorGenerator()
+    if (target.tagName !== 'BUTTON') return;
 
-        }
+    let color = target.textContent?.trim().toLowerCase();
+    if (!color) return;
+
+    // fixedColors:String[]
+
+    const fixedColors: string[] = ['green', 'blue', 'red', 'yellow'];
+
+    if (fixedColors.indexOf(color) !== -1) {
+        console.log((fixedColors.indexOf(color)));
+
+        document.body.style.backgroundColor = color;
+    } else {
+        document.body.style.backgroundColor = randomColorGenerator();
     }
 })
+
